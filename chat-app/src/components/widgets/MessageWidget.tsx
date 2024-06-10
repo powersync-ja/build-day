c6929156a910a100c385316b1480d5eaeff4c61f
DominicGBauer <dominic@nomanini.com>
2024-06-10 18:35:47 +0200

    chore: improve styling

--- chat-app/src/components/widgets/MessageWidget.tsx
+++ chat-app/src/components/widgets/MessageWidget.tsx
@@ -1,52 +1,63 @@
 import React from 'react';
-import {
-  ListItem,
-  IconButton,
-  ListItemAvatar,
-  Avatar,
-  ListItemText,
-  Box,
-  Paper,
-  styled,
-  ListItemButton
-} from '@mui/material';
+import { ListItem, IconButton, ListItemText, Box, Paper, styled, ListItemButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/DeleteOutline';
-import ListIcon from '@mui/icons-material/ListAltOutlined';
 
 type Props = {
   name: string;
   message: string;
+  isUserMessage: boolean;
   onDelete: () => void;
 };
 
-export const MessageWidget: React.FC<Props> = ({ name, message, onDelete }) => {
+export const MessageWidget: React.FC<Props> = ({ name, message, isUserMessage, onDelete }) => {
+  const backgroundColor = isUserMessage ? 'aa00ff' : undefined;
+  const justifyContent = isUserMessage ? 'flex-end' : 'flex-start';
+  const textAlign = isUserMessage ? 'right' : 'left';
+  const left = isUserMessage ? undefined : '0.6rem';
+  const right = isUserMessage ? '0.6rem' : undefined;
+
   return (
-    <S.MainPaper elevation={1}>
-      <ListItem
-        disablePadding
-        secondaryAction={
-          <Box>
-            <IconButton edge="end" aria-label="delete" onClick={onDelete}>
-              <DeleteIcon />
-            </IconButton>
-          </Box>
-        }
+    <div style={{ width: '100%', justifyContent, display: 'flex' }}>
+      <S.MainPaper
+        elevation={1}
+        sx={{
+          backgroundColor,
+          width: '90%',
+          borderRadius: '10px',
+          '&::after': {
+            content: "''",
+            display: 'inline-block',
+            width: '1rem',
+            height: '0.7rem',
+            backgroundColor,
+            position: 'absolute',
+            bottom: '-0.7rem',
+            left,
+            right,
+            clipPath: 'polygon(0 0, 100% 0, 50% 100%)'
+          }
+        }}
       >
-        <ListItemButton>
-          <ListItemAvatar>
-            <Avatar>
-              <ListIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary={message} secondary={name} />
-        </ListItemButton>
-      </ListItem>
-    </S.MainPaper>
+        <ListItem>
+          <ListItemButton>
+            {isUserMessage ? (
+              <Box>
+                <IconButton edge="end" aria-label="delete" onClick={onDelete}>
+                  <DeleteIcon />
+                </IconButton>
+              </Box>
+            ) : undefined}
+            <ListItemText primary={message} secondary={name} sx={{ textAlign }} />
+          </ListItemButton>
+        </ListItem>
+      </S.MainPaper>
+    </div>
   );
 };
 
 export namespace S {
   export const MainPaper = styled(Paper)`
-    margin-bottom: 10px;
+    margin-bottom: 16px;
+    position: relative;
   `;
 }
--- chat-app/src/components/widgets/MessagesWidget.tsx
+++ chat-app/src/components/widgets/MessagesWidget.tsx
@@ -1,13 +1,10 @@
-import { MESSAGES_ROUTE } from '@/app/router';
-import { MESSAGES_TABLE, Messages, USERS_TABLE } from '@/library/powersync/AppSchema';
+import { MESSAGES_TABLE, Messages, USERS_TABLE, Users } from '@/library/powersync/AppSchema';
 import { List } from '@mui/material';
 import { usePowerSync, useQuery } from '@powersync/react';
-import { useNavigate } from 'react-router-dom';
 import { MessageWidget } from './MessageWidget';
 
 export function MessagesWidget() {
   const powerSync = usePowerSync();
-  const navigate = useNavigate();
 
   const { data: messages } = useQuery<Messages>(`
       SELECT
@@ -15,6 +12,14 @@ export function MessagesWidget() {
       FROM
         ${MESSAGES_TABLE}
       `);
+
+  const { data: users } = useQuery<Users>(`
+      SELECT
+        ${USERS_TABLE}.*
+      FROM
+        ${USERS_TABLE}
+      `);
+  const user = users?.[0];
 
   const deleteMessage = async (id: string) => {
     await powerSync.writeTransaction(async (db) => {
@@ -29,6 +34,7 @@ export function MessagesWidget() {
         <MessageWidget
           key={message.id}
           name={message.name ?? ''}
+          isUserMessage={user?.name === message.name}
           message={message.message!}
           onDelete={() => deleteMessage(message.id)}
         />
