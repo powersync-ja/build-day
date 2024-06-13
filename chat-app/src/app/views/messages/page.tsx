001dfa78039b22e4ae65592080d026999c08ac32
DominicGBauer <dominic@nomanini.com>
2024-06-13 14:23:48 +0200

    messages/page.tsx

--- chat-app/src/app/views/messages/page.tsx
+++ chat-app/src/app/views/messages/page.tsx
@@ -1,21 +1,35 @@
 import { Box } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import { NavigationPage } from '@/navigation/NavigationPage';
 import { MessagesWidget } from '@/widgets/MessagesWidget';
 import { AddMessageWidget } from '@/widgets/AddMessageWidget';
+import { usePowerSync, useQuery } from '@powersync/react';
+import { MESSAGES_TABLE, USERS_TABLE, User, Message } from '../../../powersync/AppSchema';
 
 export default function MessagesPage() {
-  // Basic user and messages data
-  // Will be changed to use PowerSync
-  const user = { name: 'John Doe' };
-  const [messages, setMessages] = useState([{ id: '1', name: 'John Doe', message: 'Hello World' }]);
+  const powersync = usePowerSync();
+  const { data: users } = useQuery<User>(`SELECT * FROM ${USERS_TABLE}`);
+  const user = users?.[0];
+
+  const { data: messages } = useQuery<Message>(`SELECT * FROM ${MESSAGES_TABLE}`);
 
   const deleteMessage = async (id: string) => {
-    setMessages((prev) => prev.filter((message) => message.id !== id));
+    await powersync.writeTransaction(async (tx) => {
+      await tx.execute(`DELETE FROM ${MESSAGES_TABLE} WHERE id = ?`, [id]);
+    });
   };
 
   const createMessage = async (message: string) => {
-    setMessages((prev) => [...prev, { id: `${prev.length + 1}`, name: user.name, message }]);
+    const randomGroup = Math.random() > 0.5 ? 1 : 2;
+
+    const result = await powersync.execute(
+      `INSERT INTO ${MESSAGES_TABLE} (id, created_at, message, name, "group") VALUES (uuid(), datetime(), ?, ?, ?) RETURNING *`,
+      [message, user.name, randomGroup]
+    );
+    const record = result.rows?.item(0);
+    if (!record) {
+      throw new Error('Failed to create message');
+    }
   };
 
   return (
