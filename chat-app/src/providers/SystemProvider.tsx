7ff0896a0c6ea07687e515d3ae9f7820d65c360c
DominicGBauer <dominic@nomanini.com>
2024-06-13 14:23:19 +0200

    SystemProvider.tsx

--- chat-app/src/providers/SystemProvider.tsx
+++ chat-app/src/providers/SystemProvider.tsx
@@ -1,18 +1,35 @@
 import { NavigationPanelContextProvider } from '@/navigation/NavigationPanelContext';
 import { CircularProgress } from '@mui/material';
+import { WASQLitePowerSyncDatabaseOpenFactory } from '@powersync/web';
 import Logger from 'js-logger';
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
+import { AppSchema } from '../powersync/AppSchema';
+import { PowerSyncContext } from '@powersync/react';
+
+const db = new WASQLitePowerSyncDatabaseOpenFactory({
+  dbFilename: 'example.db',
+  schema: AppSchema
+}).getInstance();
+
 
 export const SystemProvider = ({ children }: { children: React.ReactNode }) => {
+  const [powersync] = useState(db);
+
   React.useEffect(() => {
     // Linting thinks this is a hook due to it's name
     Logger.useDefaults(); // eslint-disable-line
     Logger.setLevel(Logger.DEBUG);
-  }, []);
+
+    (window as any).powersync = powersync;
+
+    powersync.init();
+  }, [powersync]);
 
   return (
     <Suspense fallback={<CircularProgress />}>
-      <NavigationPanelContextProvider>{children}</NavigationPanelContextProvider>
+      <PowerSyncContext.Provider value={powersync}>
+          <NavigationPanelContextProvider>{children}</NavigationPanelContextProvider>
+      </PowerSyncContext.Provider>
     </Suspense>
   );
 };
