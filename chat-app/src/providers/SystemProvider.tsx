31ca308dbdaab7db5ed9e9394a365400ac29ed3f
DominicGBauer <dominic@nomanini.com>
2024-06-13 14:30:56 +0200

    SystemProvider.tsx-2

--- chat-app/src/providers/SystemProvider.tsx
+++ chat-app/src/providers/SystemProvider.tsx
@@ -5,15 +5,20 @@ import Logger from 'js-logger';
 import React, { Suspense, useState } from 'react';
 import { AppSchema } from '../powersync/AppSchema';
 import { PowerSyncContext } from '@powersync/react';
+import { DemoConnector } from '../powersync/DemoConnector';
 
 const db = new WASQLitePowerSyncDatabaseOpenFactory({
   dbFilename: 'example.db',
   schema: AppSchema
 }).getInstance();
 
+const ConnectorContext = React.createContext<DemoConnector | null>(null);
+export const useConnect = () => React.useContext(ConnectorContext);
 
 export const SystemProvider = ({ children }: { children: React.ReactNode }) => {
   const [powersync] = useState(db);
+
+  const [connector] = useState(new DemoConnector());
 
   React.useEffect(() => {
     // Linting thinks this is a hook due to it's name
@@ -23,12 +28,15 @@ export const SystemProvider = ({ children }: { children: React.ReactNode }) => {
     (window as any).powersync = powersync;
 
     powersync.init();
-  }, [powersync]);
+    powersync.connect(connector);
+  }, [powersync, connector]);
 
   return (
     <Suspense fallback={<CircularProgress />}>
       <PowerSyncContext.Provider value={powersync}>
+        <ConnectorContext.Provider value={connector}>
           <NavigationPanelContextProvider>{children}</NavigationPanelContextProvider>
+        </ConnectorContext.Provider>
       </PowerSyncContext.Provider>
     </Suspense>
   );
